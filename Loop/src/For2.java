0039df7d230db75488e6617684f963b42b359a4f
vkdl35 <106090523+vkdl35@users.noreply.github.com>
2022-05-25 18:09:28 +0900

    Add files via upload

---
+++ Loop/src/For2.java
@@ -0,0 +1,74 @@
+
+public class For2 {
+
+	public static void main(String[] args) {
+		// 연산 for문
+		//int a; 		//for문 초기값
+		//int b=2; 	//연산에 사용할 변수
+		//int c; 		//연산한 결과값 사용
+		//int d=9;
+		
+//		for(a=1;a<=d;a++) {
+//			c = b * a;		//산술연산 작업
+//			System.out.print(c+" "); 	//연산된 결과값을 출력함
+//		}
+//		
+		/*
+		for(a=1;a<=d;a++) {
+			System.out.print(b * a + " ");
+		}
+		*/
+		
+		/*
+		 응용문제 다음 결과값을 보고 코드를 작성하세요
+		 
+		 45,40,35,30,25,20
+		 */
+		
+		/*
+		byte aa;		//for 변수값
+		byte bb=5;		//5배수값
+		int cc;			//계산값
+		
+		for(aa=9;aa>=4;aa--) { 		//(aa=9;a>3;aa--)
+			cc = bb * aa;
+			System.out.print(cc+", ");
+		}
+		*/
+		
+		
+		/*
+		 응용문제 다음 결과값을 확인 후 코드를 제작하여 출력하시이ㅗ 
+		 
+		 11, 22, 33, 44, 55, 66, 77, 88, 99
+		 */
+		
+		/*
+		short a1;
+		short b1=11;
+		int c1;
+		
+		for(a1=1;a1<10;a1++) {
+			c1 = a1 * b1;
+			System.out.print(c1+",");
+		}
+		*/
+		
+		byte z; //for
+		int x; //결과값
+		
+		for(z=1;z<10;z++) {
+			//x = (10*z)+z;
+			x = 11 * z;
+			System.out.print(x+" ");
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Loop/src/For_plus.java
@@ -0,0 +1,48 @@
+
+public class For_plus {
+
+	public static void main(String[] args) {
+		/*
+		 * 1~10까지 모든 숫자를 합한 결과값을 출력하시오.
+		 */
+		
+		int f;
+		int total=0;	
+		for(f=1;f<=10;f++) {	//total 변수값에 반복값을 지속적으로 증가시킴
+			//total = total + f;
+			total += f;
+		}
+		System.out.println("총합계:"+total);	//합계치는 반복문 밖에 찍어야 정상 출력됨
+		//+=(더하기할당) -=(빼기할당) *=(곱하기할당) /=(나누기할당)
+		// b = b - f; -> b -= f;
+		
+		
+		/*
+		 * 응용문제
+		 * 전체값은 500이 있습니다. 총 8회 동안 반복되면서 
+		 * 한번 반복할 때마다 12씩 감소하도록 하여 최종 값을 출력하시오.
+		 * 결과값은 404
+		 */
+		
+		int a;
+		int b = 12;
+		int total2=500;
+		for(a=1;a<=8;a++) {
+			total2 = total2 - b;
+			//total2 -= b;
+			//total2 = total2 - 12;
+			//total2 -= 12;
+		}
+		System.out.println("총합계:"+total2);
+		
+		/*
+		 * 응용문제
+		 * 구구단 2단 2*1부터 ~ 2*9까지의 총 합계를 출력하시오.
+			합계는 90
+
+		 */
+		
+
+	}
+
+}
