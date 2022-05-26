4eb08214c1462edd49a738b3d7cdbb3eefbba568
vkdl35 <106090523+vkdl35@users.noreply.github.com>
2022-05-26 18:17:16 +0900

    Add files via upload

---
+++ Example/src/Example2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		/*
+		 [응용문제]
+		 
+		 사용자가 입력한 숫자에 맞춰서 구구단이 작동되며,
+		 구구단 결과를 계산하여 짝수인지 홀수인지를 출력하시오.
+		 "구구단에 대한 숫자를 입력하세요." 라고 메세지가 출력되면
+		 해당 숫자에 맞춰서 모든 결과를 더하며,
+		 더한 값에 따라 짝수인지 홀수인지를 출력하시면 됩니다.
+		 단, 해당 반복문은 for ~ dowhile문까지
+		 한가지를 선택해서 작성하시오.
+		 
+		 **조건문은 반복문 안에 있으면 안됩니다.
+		 */
+		
+		Scanner aa = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
