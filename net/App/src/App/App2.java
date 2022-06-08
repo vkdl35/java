e9e1b1e6436aff85a79e103fa56e015fd2eb5e2a
vkdl35 <106090523+vkdl35@users.noreply.github.com>
2022-06-08 18:16:03 +0900

    Add files via upload

---
+++ net/App/src/App/App2.java
@@ -0,0 +1,37 @@
+package App;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
+
+class Adata {
+	
+	private String[][] u;
+	
+	public Adata() {
+		String user[][] = { 
+				{ "박은경", "이경훈", "장운", "조기현", "김진수" }, 
+				{ "80", "78", "92", "67", "40" },
+				{ "40", "80", "80", "92", "100" }, 
+				{ "95", "30", "55", "90", "65" },
+				{ "20", "80", "100", "95", "30" } 
+		};
+		this.u = user;
+	}
+	
+	public String Bdata() {
+		
+		return null;
+	}
+	
+	
+	
+	
+}
