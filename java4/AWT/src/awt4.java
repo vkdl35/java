f9b4d1678b8bd0055a16860ffcc1b8f004cf30ee
vkdl35 <106090523+vkdl35@users.noreply.github.com>
2022-06-15 18:24:51 +0900

    Add files via upload

--- java4/AWT/src/awt4.java
+++ java4/AWT/src/awt4.java
@@ -5,26 +5,30 @@ public class awt4 {
 		// awt4_class.java와 연계
 		decorate de = new decorate();
 		de.view();
+		
 	}
 
 }
 
 class decorate extends awt4_class {
-	public String 
+	public String aws="";
 	public void btn_push(int c) {
 		/*사용자가 입력하는 숫자를 기준으로 구구단을 작성하세요*/
 		//System.out.println(c);
 		int w = 1;
 		int total = 1;
+		String prf = "";
 		while(w < 10) {
 			total = w * c;
-			System.out.printf("%d*%d=%d ",c,w,total);
+			//System.out.printf("%d*%d=%d ",c,w,total);
+			prf = prf + c + "*" + w + "=" + total + "\n";
 			w++;
 		}
-		
+		this.aws = prf;
 	}
-	public int calls() {
-		return ;
+	public String calls() {
+		return this.aws;
 	}
 }
+
 
--- java4/AWT/src/awt4_class.java
+++ java4/AWT/src/awt4_class.java
@@ -1,14 +1,13 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 public class awt4_class {
-	//Scanner sc = new Scanner(System.in);
+
 	// awt4.java와 연계
 	public void view() {
 		decorate de = new decorate();	//awt4.java 자식 클래스 로드
 		Frame f = new Frame();
 
-		Label lb = new Label();
+		TextArea lb = new TextArea();
 
 		f.setBounds(500, 200, 300, 300);
 		f.setVisible(true);
@@ -37,8 +36,8 @@ public class awt4_class {
 				try {
 					int c = Integer.parseInt(input.getText());
 					//lb.setText(de.btn_push(c));
-					de.btn_push(c);
-					 de.calls();
+					de.btn_push(c);	//사용자가 입력하는 값을 btn_push 메소드로 전달
+					lb.setText(de.calls());	//awt4.java에 있는 getter 메소드로 값을 받음
 				}catch (Exception c) {
 					lb.setText("숫자만 입력하세요");
 					
