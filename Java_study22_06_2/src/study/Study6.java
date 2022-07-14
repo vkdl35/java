954a59004f373a948ab7b359c93c29ed19712292
vkdl35 <106090523+vkdl35@users.noreply.github.com>
2022-07-14 18:01:40 +0900

    Add files via upload

--- Java_study22_06_2/src/study/Study6.java
+++ Java_study22_06_2/src/study/Study6.java
@@ -1,49 +1,50 @@
 package study;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
-import java.util.Set;
 
 public class Study6 {
-	
+
 	public static void main(String[] args) {
-		
-		Study6 s6 = new Study6();
-		System.out.println(s6.name());
-		System.out.println(s6.name2());
-		
-	}
-	
-	public ArrayList<Integer> name() {
-		ArrayList<Integer> ar = new ArrayList<Integer>();
-		ArrayList<Integer> ar2 = new ArrayList<Integer>();
-		int w = 1;
-		while(w < 11) {
-			ar.add((int)(Math.random()*10)+1);
-			for (int f : ar) {
-				if(!ar2.contains(f)) {
-					ar2.add(f);
-				}
-			}
-			w++;
-		}
-		
-		return ar2;
-	}
-	
-	public ArrayList<Integer> name2() {
-		Set<Integer> set = new LinkedHashSet<>();
-		while(set.size() < 10) {
-			int d = (int)(Math.random()*10)+1;
-			set.add(d);
-		}
-		ArrayList<Integer> ar = new ArrayList<>(set);
-		return ar;
+
+	      Integer[] numberList = {1,2,6,7,10,11,15,22,42,43,50,51};
+	      
+	      
+	      calculation ca = new calculation(numberList,true);
+	      calculation ca2 = new calculation(numberList,false);
+	      ca.run();
+	      ca2.run();
+	   }   
 	}
 
-}
+	class calculation implements Runnable{
+	   
+	   List<Integer>numberList = new ArrayList<>();
+	   List<Integer>collector = new ArrayList<>();
+	   boolean isEven;
+	   
+	   public calculation(Integer[] number,boolean isEven) {
+	      
+	      this.isEven = isEven;
+	      this.numberList.addAll(Arrays.asList(number));
+	   }
+	   
+	   @Override
+	   public void run() {
+	      
+	      for(int i = 0; i < numberList.size(); i++) {
+	         if(isEven && this.numberList.get(i)%2 == 0 ) {
+	            this.collector.add(this.numberList.get(i));
+	         }
+	         
+	         if(!isEven && this.numberList.get(i)%2 != 0) {
+	            this.collector.add(this.numberList.get(i));
+	         }
+	      }
+	      System.out.println(this.collector);
+	   }
+	}
+
+
+
