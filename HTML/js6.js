1edd5a2d8d799641a4eda942a831c810d2021fdd
vkdl35 <106090523+vkdl35@users.noreply.github.com>
2022-07-17 13:32:36 +0900

    Delete HTML directory

--- HTML/js6.js
+++
@@ -1,9 +0,0 @@
-function call1() {
-    var aa;
-    var bb = 0;
-    for(aa=1;aa<11;aa++){
-        bb += aa;
-    }
-    var result = return1(bb);
-    console.log(result+"입니다.");
-}
--- HTML/js8_1.js
+++
@@ -1,7 +0,0 @@
-data_c=["S","V","N","N"];
-
-function call(n){
-    var grade = data_c[n];
-
-    return(grade);
-}
